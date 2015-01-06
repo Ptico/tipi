9fc6442fc4be4ceea9a5705e73530b696b97977d
Andrey Savchenko <andrey@aejis.eu>
2015-01-06 17:16:39 +0200

    Implement Status

--- lib/tipi/status.rb
+++ lib/tipi/status.rb
@@ -8,10 +8,119 @@ module Tipi
       5 => :server_error
     }.freeze
 
+    @registry = {}
+
     class << self
+      def all
+        @registry.values
+      end
+
+      def [](status)
+        @registry[status]
+      end
+
+      def fetch(status, *args, &block)
+        @registry.fetch(status.to_i, *args, &block)
+      end
+
+      def register(status, name, cacheable: false, allows_body: true)
+        status = status.to_i
+
+        @registry[status] = new(status, name, cacheable, allows_body)
+      end
+
       def type(status)
         TYPES[status.to_i / 100]
       end
     end
+
+    attr_reader :code
+
+    attr_reader :name
+
+    attr_reader :type
+
+    def allows_body?
+      @allows_body
+    end
+
+    def cacheable?
+      @cacheable
+    end
+
+  private
+
+    def initialize(code, name, cacheable, allows_body)
+      @code = code
+      @name = name
+      @type = self.class.type(code)
+
+      @cacheable   = cacheable
+      @allows_body = allows_body
+    end
+
   end
+
+  Status.register(100, 'Continue', allows_body: false)
+  Status.register(101, 'Switching Protocols', allows_body: false)
+  Status.register(102, 'Processing', allows_body: false)
+
+  Status.register(200, 'OK', cacheable: true)
+  Status.register(201, 'Created')
+  Status.register(202, 'Accepted')
+  Status.register(203, 'Non-Authoritative Information', cacheable: true)
+  Status.register(204, 'No Content', cacheable: true, allows_body: false)
+  Status.register(205, 'Reset Content', allows_body: false)
+  Status.register(206, 'Partial Content', cacheable: true)
+  Status.register(207, 'Multi-Status')
+  Status.register(208, 'Already Reported')
+
+  Status.register(300, 'Multiple Choices', cacheable: true)
+  Status.register(301, 'Moved Permanently', cacheable: true)
+  Status.register(302, 'Found')
+  Status.register(303, 'See Other')
+  Status.register(304, 'Not Modified', allows_body: false)
+  Status.register(305, 'Use Proxy')
+  Status.register(306, 'Reserved')
+  Status.register(307, 'Temporary Redirect')
+  Status.register(308, 'Permanent Redirect')
+
+  Status.register(400, 'Bad Request')
+  Status.register(401, 'Unauthorized')
+  Status.register(402, 'Payment Required')
+  Status.register(403, 'Forbidden')
+  Status.register(404, 'Not Found', cacheable: true)
+  Status.register(405, 'Method Not Allowed', cacheable: true)
+  Status.register(406, 'Not Acceptable')
+  Status.register(407, 'Proxy Authentication Required')
+  Status.register(408, 'Request Timeout')
+  Status.register(409, 'Conflict')
+  Status.register(410, 'Gone', cacheable: true)
+  Status.register(411, 'Length Required')
+  Status.register(412, 'Precondition Failed')
+  Status.register(413, 'Request Entity Too Large')
+  Status.register(414, 'Request-URI Too Long', cacheable: true)
+  Status.register(415, 'Unsupported Media Type')
+  Status.register(416, 'Requested Range Not Satisfiable')
+  Status.register(417, 'Expectation Failed')
+  Status.register(422, 'Unprocessable Entity')
+  Status.register(423, 'Locked')
+  Status.register(424, 'Failed Dependency')
+  Status.register(426, 'Upgrade Required')
+  Status.register(428, 'Precondition Required')
+  Status.register(429, 'Too Many Requests')
+  Status.register(431, 'Request Header Fields Too Large')
+  Status.register(451, 'Unavailable For Legal Reasons')
+
+  Status.register(500, 'Internal Server Error')
+  Status.register(501, 'Not Implemented', cacheable: true)
+  Status.register(502, 'Bad Gateway')
+  Status.register(503, 'Service Unavailable')
+  Status.register(504, 'Gateway Timeout')
+  Status.register(505, 'HTTP Version Not Supported')
+  Status.register(506, 'Variant Also Negotiates (Experimental)')
+  Status.register(507, 'Insufficient Storage')
+  Status.register(508, 'Loop Detected')
+  Status.register(510, 'Not Extended')
+  Status.register(511, 'Network Authentication Required')
 end
--- spec/unit/tipi/status_spec.rb
+++ spec/unit/tipi/status_spec.rb
@@ -1,6 +1,49 @@
 RSpec.describe Tipi::Status do
   describe '.[]' do
+    subject { described_class[status] }
 
+    context 'when status is an integer' do
+      let(:status) { [200, 201, 302, 404].shuffle.first }
+
+      it { expect(subject.code).to equal(status) }
+    end
+
+    context 'when status is a string' do
+      let(:status) { '200' }
+
+      it { expect(subject).to be(nil) }
+    end
+  end
+
+  describe '.fetch' do
+    subject { described_class.fetch(status) }
+
+    context 'when status is an integer' do
+      let(:status) { [100, 202, 301, 403].shuffle.first }
+
+      it { expect(subject.code).to eql(status) }
+    end
+
+    context 'when status is a string' do
+      let(:status) { %w(200 307 401 502).shuffle.first }
+
+      it { expect(subject.code).to eql(status.to_i) }
+    end
+
+    context 'when status is not present' do
+      let(:status) { 666 }
+
+      it { expect { subject }.to raise_error(KeyError) }
+    end
+
+    context 'when status is not present but fallback given' do
+      subject { described_class.fetch(status, default) }
+
+      let(:status)  { 666 }
+      let(:default) { described_class[200] }
+
+      it { expect(subject).to eql(default) }
+    end
   end
 
   describe '.valid?' do
@@ -41,55 +84,75 @@ RSpec.describe Tipi::Status do
     end
   end
 
-  describe '#allows_entity_body?' do
+  describe '#code' do
+    it { expect(described_class[200].code).to equal(200) }
+    it { expect(described_class[404].code).to equal(404) }
+  end
+
+  describe '#name' do
+    it { expect(described_class[200].name).to eql('OK') }
+    it { expect(described_class[301].name).to eql('Moved Permanently') }
+  end
+
+  describe '#type' do
+    it { expect(described_class[100].type).to equal(:informational) }
+    it { expect(described_class[201].type).to equal(:successful) }
+    it { expect(described_class[301].type).to equal(:redirection) }
+    it { expect(described_class[429].type).to equal(:client_error) }
+    it { expect(described_class[504].type).to equal(:server_error) }
+  end
+
+  describe '#allows_body?' do
     let(:prohibited_statuses) { [100, 101, 102, 204, 205, 304] }
 
     context 'when entity body prohibited' do
       let(:statuses) do
-        Tipi::Status.all.select { |s| prohibited_statuses.include?(s.code) }
+        described_class.all.select { |s| prohibited_statuses.include?(s.code) }
       end
 
       it 'is false' do
         statuses.each do |status|
-          expect(status.allows_entity_body?).to be(false)
+          expect(status.allows_body?).to be(false)
         end
       end
     end
 
     context 'when entity body allowed' do
       let(:statuses) do
-        Tipi::Status.all.reject { |s| prohibited_statuses.include?(s.code) }
-      end
-
-      it 'is true' do
-        expect(status.allows_entity_body?).to be(true)
-      end
-    end
-  end
-
-  describe '#allows_caching?' do
-    let(:allowed_statuses) { Set[200, 203, 204, 206, 300, 301, 404, 405, 410, 414, 501] }
-
-    context 'when caching allowed' do
-      let(:statuses) do
-        Tipi::Status.all.select { |s| allowed_statuses.include?(s.code)}
+        described_class.all.reject { |s| prohibited_statuses.include?(s.code) }
       end
 
       it 'is true' do
         statuses.each do |status|
-          expect(status.allows_caching?).to be(true)
+          expect(status.allows_body?).to be(true)
+        end
+      end
+    end
+  end
+
+  describe '#cacheable?' do
+    let(:allowed_statuses) { Set[200, 203, 204, 206, 300, 301, 404, 405, 410, 414, 501] }
+
+    context 'when caching allowed' do
+      let(:statuses) do
+        described_class.all.select { |s| allowed_statuses.include?(s.code)}
+      end
+
+      it 'is true' do
+        statuses.each do |status|
+          expect(status).to be_cacheable
         end
       end
     end
 
     context 'when caching prohibited' do
       let(:statuses) do
-        Tipi::Status.all.reject { |s| allowed_statuses.include?(s.code)}
+        described_class.all.reject { |s| allowed_statuses.include?(s.code)}
       end
 
       it 'is false' do
         statuses.each do |status|
-          expect(status.allows_caching?).to be(false)
+          expect(status).to_not be_cacheable
         end
       end
     end
